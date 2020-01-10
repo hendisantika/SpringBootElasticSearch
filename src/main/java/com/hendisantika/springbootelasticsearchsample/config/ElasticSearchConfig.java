51770f991749e2e5d437d6b039894030c3d80f30
Hendi Santika <hendisantika@gmail.com>
2020-01-11 06:13:55 +0700

    #7 - Add client in ElasticSearchConfig

--- src/main/java/com/hendisantika/springbootelasticsearchsample/config/ElasticSearchConfig.java
+++ src/main/java/com/hendisantika/springbootelasticsearchsample/config/ElasticSearchConfig.java
@@ -1,7 +1,11 @@
 package com.hendisantika.springbootelasticsearchsample.config;
 
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 /**
@@ -26,4 +30,14 @@ public class ElasticSearchConfig {
     @Value("${elasticsearch.clustername}")
     private String EsClusterName;
 
+    @Bean
+    RestHighLevelClient client() {
+
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200", "localhost:9300")
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
 }
