af42feb40bd3f6c4b92c6ab8f291a57ad25a20a3
Hendi Santika <hendisantika@gmail.com>
2020-01-11 06:00:57 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootelasticsearchsample/SpringbootElasticsearchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootelasticsearchsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticsearchSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootElasticsearchSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootelasticsearchsample/SpringbootElasticsearchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootelasticsearchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearchSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
