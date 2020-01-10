00dac3997f1b76f4bef8ecde894ac72703bf173c
Hendi Santika <hendisantika@gmail.com>
2020-01-11 06:11:23 +0700

    #5 - Add EmployeeDocument Repository

---
+++ src/main/java/com/hendisantika/springbootelasticsearchsample/repository/EmployeDocumentRepository.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootelasticsearchsample.repository;
+
+import com.hendisantika.springbootelasticsearchsample.domain.EmployeeDocument;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-elasticsearch-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/01/20
+ * Time: 06.10
+ */
+public interface EmployeDocumentRepository extends ElasticsearchRepository<EmployeeDocument, String> {
+    List<EmployeeDocument> findByDocTitleEndsWith(String name);
+
+    List<EmployeeDocument> findByDocTitleStartsWith(String name);
+
+    List<EmployeeDocument> findByDocTypeEndsWith(String name);
+
+    List<EmployeeDocument> findByDocTypeStartsWith(String name);
+}
