30427ca98fb330c6fe707f75bb517ea08541598b
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 07:23:56 +0700

    Add Spring Doc Config🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootelasticsearchsample/config/OpenAPIConfig.java
@@ -0,0 +1,38 @@
+package com.hendisantika.springbootelasticsearchsample.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : SpringBootElasticSearch
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/06/24
+ * Time: 07.14
+ * To change this template use File | Settings | File Templates.
+ */
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI notifOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.info(new io.swagger.v3.oas.models.info.Info()
+                .title("Invoice Service API")
+                .description(appDescription)
+                .version(appVersion)
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                        .name("Hendi Santika")
+                        .url("https://s.id/hendisantika")
+                        .email("hendisantika@yahoo.co.id"))
+                .termsOfService("TOC")
+                .license(new io.swagger.v3.oas.models.info.License().name("License").url("https://s.id/hendisantika")));
+        return openAPI;
+    }
+}
